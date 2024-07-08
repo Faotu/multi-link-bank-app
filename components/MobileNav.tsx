b16aabb469ff0d88ee3e4e2bd5e7e0ea72dda75a
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2024-07-08 22:17:31 +0100

    mobile side bar layout

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,7 +1,93 @@
+"use client";
 import React from "react";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
 
 const MobileNav = ({ user }: MobileNavProps) => {
-  return <div>Nav</div>;
+  const pathname = usePathname();
+  return (
+    <section className="w-full mx-w-[2644px]">
+      <Sheet>
+        <SheetTrigger>
+          <Image
+            src="/icons/hamburger.svg"
+            width={30}
+            height={30}
+            alt="menu"
+            className="cursor-point"
+          />
+        </SheetTrigger>
+        <SheetContent side="left" className="border-none bg-white">
+          <Link
+            href="/"
+            className="cursor-pointer flex items-center gap-1 px-4"
+          >
+            <Image
+              src="/icons/logo.svg"
+              width={34}
+              height={34}
+              alt="Banking logo"
+            />
+            <h1 className="text-26 font-ibm-plex-serif font-bold">Banking</h1>
+          </Link>
+
+          <div className="mobilenav-sheet">
+            <SheetClose asChild>
+              <nav className="flex h-full flex-col gap-4 pt-16 text-white">
+                {sidebarLinks.map((item) => {
+                  const isActive =
+                    pathname === item.route ||
+                    pathname.startsWith(`{item.route}/`);
+                  return (
+                    <SheetClose asChild key={item.route}>
+                      <Link
+                        href={item.route}
+                        key={item.label}
+                        className={cn("mobilenav-sheet_close", {
+                          "bg-bankGradient": isActive,
+                        })}
+                      >
+                        <div className="relative size-6">
+                          <Image
+                            src={item.imgURL}
+                            alt={item.label}
+                            fill
+                            className={cn({
+                              "brightness-[3] invert-0": isActive,
+                            })}
+                          />
+                        </div>
+                        <p
+                          className={cn("sidebar-label", {
+                            "!text-white": isActive,
+                          })}
+                        >
+                          {" "}
+                          {item.label}
+                        </p>
+                      </Link>
+                    </SheetClose>
+                  );
+                })}
+              </nav>
+            </SheetClose>
+          </div>
+        </SheetContent>
+      </Sheet>
+    </section>
+  );
 };
 
 export default MobileNav;
